25162eb2ae24d6d1f02b699520765159dd1606e2
Avik Agarwala <98759608+AvikAgarwala@users.noreply.github.com>
2023-07-26 14:25:19 +0530

    Delete Input-Output directory

--- Input-Output/cout-2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main()
-{
-    int x = 5, y = 6;
-    std::cout << "Value of x is " << x << std::endl;
-    std::cout << "x = " << x << ", y = " << y << std::endl;
-
-    return 0;
-}
