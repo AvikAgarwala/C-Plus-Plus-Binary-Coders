512dad4cc6a3b7ff6a7a7eeaf2911495c7ae5f85
Avik Agarwala <avikagarwala@outlook.com>
2023-07-26 15:14:47 +0530

    create string.cpp

---
+++ string/string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str = "Binary Coders";
+    cout << str << endl;
+
+    cin >> str; // blank space works as terminating character
+    cout << str << endl;
+
+    return 0;
+}
