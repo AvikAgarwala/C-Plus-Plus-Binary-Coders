17602ff710a09a2db7e2d6b2c727bb4ff4e2b43f
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-07-27 00:49:48 +0530

    Update vector-3.cpp

--- vector/vector-3.cpp
+++ vector/vector-3.cpp
@@ -9,11 +9,11 @@ int main()
     v.push_back(2);
     v.push_back(3);
 
-    cout << v.empty() << endl;
+    cout << v.empty() << endl; //output 0 and 0 means not empty
 
     v.clear();
 
-    cout << v.empty() << endl;
+    cout << v.empty() << endl; // output 1 and 1 means empty
     
     return 0;
 }
