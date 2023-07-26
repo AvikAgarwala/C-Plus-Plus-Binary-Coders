4cd0ca64f3156dae1e6ebe6bbbb14702ccf37f7b
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-07-26 23:47:08 +0530

    Update cin-2.cpp
    I am using cin operator just once to take the inputs of both x and y simultaneously

--- input-output/cin-2.cpp
+++ input-output/cin-2.cpp
@@ -3,12 +3,6 @@ using namespace std;
 int main()
 {
     int x, y;
-
-    cin >> x;
-    cin >> y;
-    cout << "Value of x is " << x << endl;
-    cout << "Value of y is " << y << endl;
-
     cin >> x >> y;
     cout << "Value of x is " << x << endl;
     cout << "Value of y is " << y << endl;
