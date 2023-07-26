77ca1e278b25f7d77d505101572f1c1f882c6464
Avik Agarwala <avikagarwala@outlook.com>
2023-07-26 13:14:13 +0530

    Create cin-1.cpp

---
+++ Input-Output/cin-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout << "Enter value for x : ";
+    cin >> x;
+    cout << "Value of x is " << x << endl;
+    
+    return 0;
+}
