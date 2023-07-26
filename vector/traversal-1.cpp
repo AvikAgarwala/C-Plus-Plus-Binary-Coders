5963ac1124d78f828e053b1c2d55610819cd4933
Alik Agarwala <alikagarwala@outlook.com>
2023-07-26 22:38:16 +0530

    Create vector / traversal

---
+++ vector/traversal-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    for (auto i : v) // using iterator
+        cout << i << " ";
+    return 0;
+}
---
+++ vector/traversal-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    for (int i = 0; i < v.size(); i++) // using size()
+        cout << v[i] << " ";
+    return 0;
+}
