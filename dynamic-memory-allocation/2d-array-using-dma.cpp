0ea317a3fb53cad067d7d86b0e1c7ffaaa5f9b99
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-07-27 23:46:24 +0530

    Update 2d-array-using-dma.cpp

--- dynamic-memory-allocation/2d-array-using-dma.cpp
+++ dynamic-memory-allocation/2d-array-using-dma.cpp
@@ -3,27 +3,27 @@ using namespace std;
 int main()
 {
     int row, col;
-    cin >> row >> col;
-
-    int **arr = new int *[row];
+    cin >> row >> col; // taking input of row and coloumn nummber individually
+    //memory allocating for rows
+    int **arr = new int *[row]; // int** arr means pointer to pointer
 
     for (int i = 0; i < row; i++)
-        arr[i] = new int[col];
+        arr[i] = new int[col]; // memory allocating for coloumns
 
     for (int i = 0; i < row; i++)
         for (int j = 0; j < col; j++)
-            cin >> arr[i][j];
+            cin >> arr[i][j]; // taking input for the array
 
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
-            cout << arr[i][j] << " ";
+            cout << arr[i][j] << " "; // printing the whole array
         cout << endl;
     }
 
     for (int i = 0; i < row; i++)
-        delete arr[i];
-    delete arr;
+        delete arr[i]; // to deallocate the individual array to prevent memory leakage
+    delete arr; // to deallocate the main array
 
     return 0;
 }
