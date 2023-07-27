cc677fea54253211c0810b9ab300daa811e18c07
Avik Agarwala <avikagarwala@outlook.com>
2023-07-27 23:32:05 +0530

    create oop/method-outside-1.cpp

---
+++ oop/method-outside-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    void eat(); // prototype
+};
+
+void Animal::eat() // definition
+{
+    cout << "I can eat." << endl;
+}
+
+int main()
+{
+    Animal dog;
+    dog.eat();
+    return 0;
+}
