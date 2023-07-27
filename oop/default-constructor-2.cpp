cd66dab1d5d4d3db629e4de48b8f2caaa4222f7d
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 00:45:20 +0530

    create default-constructor-2.cpp

---
+++ oop/default-constructor-2.cpp
@@ -0,0 +1,28 @@
+// THIS CODE WILL GIVE AN ERROR !
+
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    string name;
+
+    // constructor
+    Student(string name, int roll)
+    {
+        this->name = name;
+        this->roll = roll;
+    }
+};
+
+int main()
+{
+    Student s1("Avik", 12);    // constructor called
+    Student s2("Poulami", 45); // constructor called
+
+    Student s3; // error due to lack of suitable constructor
+
+    return 0;
+}
