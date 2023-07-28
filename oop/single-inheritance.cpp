7474e21dfbea3c9f7928449410470dee7a84b116
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 14:38:35 +0530

    create single-inheritance.cpp

---
+++ oop/single-inheritance.cpp
@@ -0,0 +1,18 @@
+class A // base class
+{
+public:
+    int a;
+};
+
+class B : public A // derived class
+{
+public:
+    int b;
+};
+
+int main()
+{
+    // YOUR CODE GOES HERE
+
+    return 0;
+}
