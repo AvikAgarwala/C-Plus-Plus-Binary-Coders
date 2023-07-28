6a72953c758ec11497404c50bc6ba4307d0ed666
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 15:46:23 +0530

    create oop/inheritance-4.cpp

---
+++ oop/inheritance-4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class A // base class
+{
+public:
+    A()
+    {
+        cout << "class A constructor called" << endl;
+    }
+};
+
+class B : public A // derived class
+{
+public:
+    B(int temp)
+    {
+        cout << "class B constructor called" << endl;
+    }
+};
+
+int main()
+{
+    B obj(12); // create object of class B
+
+    return 0;
+}
