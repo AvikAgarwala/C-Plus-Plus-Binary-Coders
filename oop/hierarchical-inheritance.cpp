af96b3ddea69c4bfa7f418520ef3dad697935344
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 14:40:57 +0530

    create oop/hierarchical-inheritance.cpp

---
+++ oop/hierarchical-inheritance.cpp
@@ -0,0 +1,23 @@
+class A // base class
+{
+public:
+    int a;
+};
+
+class B : public A // derived class
+{
+public:
+    int b;
+};
+
+class C : public A // derived class
+{
+    
+};
+
+int main()
+{
+    // YOUR CODE GOES HERE
+
+    return 0;
+}
