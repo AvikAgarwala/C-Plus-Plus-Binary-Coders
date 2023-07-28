01524dc46e346bc479aad40d05c5f5a6584d1b13
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 14:52:57 +0530

    create oop/multiple-inheritance-2.cpp

---
+++ oop/multiple-inheritance-2.cpp
@@ -0,0 +1,29 @@
+// THIS CODE WILL GIVE AN ERROR!
+
+#include <iostream>
+using namespace std;
+
+class A // base class
+{
+public:
+    int var = 1;
+};
+
+class B // base class
+{
+public:
+    int var = 2;
+};
+
+class C : public A, public B // derived class
+{
+    
+};
+
+int main()
+{
+    C obj;
+    cout << obj.var << endl;
+
+    return 0;
+}
