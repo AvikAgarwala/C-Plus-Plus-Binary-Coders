1f0de5b959e3327ae8a66a88f4157d1a4b0200b4
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 15:51:19 +0530

    create oop/inheritance-6.cpp

---
+++ oop/inheritance-6.cpp
@@ -0,0 +1,34 @@
+// THIS CODE WILL GIVE AN ERROR!
+
+#include <iostream>
+using namespace std;
+
+class A // base class
+{
+public:
+    A(int x)
+    {
+        cout << "class A constructor called" << endl;
+    }
+
+    A() // to satisfy the call from child class B
+    {
+        cout << "call to super" << endl;
+    }
+};
+
+class B : public A // derived class
+{
+public:
+    B(int temp)
+    {
+        cout << "class B constructor called" << endl;
+    }
+};
+
+int main()
+{
+    B obj(12); // create object of class B
+
+    return 0;
+}
