82187386b9287e407d653a52b338e64c2632b136
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-07-28 20:48:44 +0530

    Update quick-sort.cpp
    Adding comments on how this quicksort algo works mainly the places where swapping and recursive calling takes place

--- sorting/quick-sort.cpp
+++ sorting/quick-sort.cpp
@@ -2,7 +2,8 @@
 #include <algorithm> // to use swap()
 using namespace std;
 
-void quickSort(int[], int, int);
+// Function declaration
+void quickSort(int[], int, int); 
 
 int partition(int[], int, int);
 
@@ -10,36 +11,36 @@ int main()
 {
     int n;
     cin >> n;
-    int arr[n];
+    int arr[n]; // input array size
 
     for (int i = 0; i < n; i++)
-        cin >> arr[i];
+        cin >> arr[i]; // input array elements
 
-    quickSort(arr, 0, n - 1);
+    quickSort(arr, 0, n - 1); // calling quicksort function
 
     for (int i : arr)
-        cout << i << " ";
+        cout << i << " "; // printing array elements
 
     return 0;
 }
 
 void quickSort(int arr[], int low, int high)
 {
-    if (low < high)
+    if (low < high) // base condition
     {
         int PI = partition(arr, low, high);
-        quickSort(arr, low, PI - 1);
-        quickSort(arr, PI + 1, high);
+        quickSort(arr, low, PI - 1); // recursive calling
+        quickSort(arr, PI + 1, high); // recursive calling
     }
 }
 
 int partition(int arr[], int low, int high)
 {
     int pivot = arr[high];
-    int PI = low - 1;
+    int PI = low - 1; // Obtaining pivot index
     for (int i = low; i < high; i++)
         if (arr[i] < pivot)
-            swap(arr[i], arr[++PI]);
-    swap(arr[high], arr[++PI]);
+            swap(arr[i], arr[++PI]); // swapping
+    swap(arr[high], arr[++PI]); // swapping
     return PI;
 }
