f3f8594f473f8b1ea1141e6566a25bea7982a94e
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 12:22:05 +0530

    create oopo/destructor-3.cpp

---
+++ oop/destructor-3.cpp
@@ -0,0 +1,32 @@
+// THIS CODE WILL GIVE AN ERROR!
+
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    // constructors
+    Student()
+    {
+    }
+    Student(int roll)
+    {
+    }
+
+    // destructor
+    ~Student(int roll)
+    {
+        cout << "destructor called" << endl;
+    }
+};
+
+int main()
+{
+    Student s1;
+    Student s2(12);
+
+    cout << "end of main()" << endl;
+
+    return 0;
+}
