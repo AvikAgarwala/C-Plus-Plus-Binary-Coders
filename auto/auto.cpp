902f340f101fcf61d7d9ac9a6f0699745c5958b4
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-07-29 13:45:22 +0530

    Update auto.cpp
    Declaring the type of x , ch and myVar in the program. Also mentioned what is ptr and pptr in this code

--- auto/auto.cpp
+++ auto/auto.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main()
 {
-    auto x = 5;
-    auto ch = 'c';
-    auto myVar = 4.56;
+    auto x = 5; // int
+    auto ch = 'c'; // char
+    auto myVar = 4.56; // double
 
     cout << x << " " << ch << " " << myVar << endl;
 
     auto v = 2;
-    auto ptr = &v;
-    auto pptr = &ptr;
-    cout << *ptr << endl;
+    auto ptr = &v; // int*
+    auto pptr = &ptr; // int**
+    cout << *ptr << endl; 
     cout << **pptr << endl;
 
     return 0;
