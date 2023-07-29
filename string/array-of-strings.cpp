6137bb676c3646809df228ecdc59d7f2aef47243
ARGHADEEP SARKAR <112500013+Arghadeeps07@users.noreply.github.com>
2023-07-30 00:47:55 +0530

    Update array-of-strings.cpp

--- string/array-of-strings.cpp
+++ string/array-of-strings.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    string arr[3] = {"Binary", "Coders", "CPP"}; // creating an array of string
+    string arr[3] = {"Binary", "Coders", "CPP"}; // creating an array of strings
 
     for (int i = 0; i < 3; i++)
         cout << arr[i] << " ";
