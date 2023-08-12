f9813e944c93892955a7cb4d7253170bbf9061ec
Alik Agarwala <alikagarwala@outlook.com>
2023-08-12 23:28:55 +0530

    create function-overloading.cpp

---
+++ oop/function-overloading.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Calculation
+{
+public:
+    int sum(int a, int b)
+    {
+        return a + b;
+    }
+    float sum(float a, float b)
+    {
+        return a + b;
+    }
+    double sum(double a, double b)
+    {
+        return a + b;
+    }
+};
+int main()
+{
+    Calculation c1;
+    cout << c1.sum(5, 6) << endl;
+    cout << c1.sum(1.1, 2.2) << endl;
+    cout << c1.sum(1.2f, 0.1f) << endl;
+    return 0;
+}
