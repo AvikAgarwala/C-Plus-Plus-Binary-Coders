a8348d73d3cf459244bcd131267be2c5e6c0fc9a
Alik Agarwala <alikagarwala@outlook.com>
2023-08-13 12:56:15 +0530

    Update operator-overloading-2.cpp

--- oop/operator-overloading-2.cpp
+++ oop/operator-overloading-2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class ComplexNumber
 {
 public:
@@ -10,16 +11,18 @@ public:
         this->real = real;
         this->imaginary = imaginary;
     }
-    ComplexNumber operator++()  // prefix operator overloading
+    ComplexNumber operator++() // prefix operator overloading
     {
         real += 2; // increment real part by 2
         return *this; 
     }
 };
+
 int main()
 {
     ComplexNumber c1(3, 7);
-    ++c1; // first increment then store
+    ++c1;
     cout << c1.real << endl;
+    
     return 0;
 }
