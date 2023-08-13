39465f35b995863801df2edfcad4b98234abad01
Alik Agarwala <alikagarwala@outlook.com>
2023-08-13 13:01:06 +0530

    Update function-overloading.cpp

--- oop/function-overloading.cpp
+++ oop/function-overloading.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class Calculation
 {
 public:
@@ -16,11 +17,14 @@ public:
         return a + b;
     }
 };
+
 int main()
 {
-    Calculation c1;
+    Calculation c1
+    
     cout << c1.sum(5, 6) << endl;
     cout << c1.sum(1.1, 2.2) << endl;
     cout << c1.sum(1.2f, 0.1f) << endl;
+    
     return 0;
 }
