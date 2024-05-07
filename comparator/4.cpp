2073af6ab6206fc6c4cd872cea5900a5b3ca840f
Alik Agarwala <alikagarwala@outlook.com>
2024-05-08 00:05:31 +0530

    4.cpp

---
+++ comparator/4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    // sorting a vector of pairs
+    // sorting based on the first element of the pair
+
+    int n;
+    cin >> n;
+
+    cout << endl;
+
+    vector<pair<int, int> > v;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        v.push_back(make_pair(x, y));
+    }
+
+    cout << endl;
+
+    sort(v.begin(), v.end()); // by default ascending order based on the first element of the pair
+
+    for (auto i : v)
+        cout << i.first << " " << i.second << endl;
+    
+
+    return 0;
+}
