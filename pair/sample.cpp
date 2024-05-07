159c5033103f9f6e12641caf3c99f75b0db44281
Alik Agarwala <alikagarwala@outlook.com>
2024-05-07 23:20:04 +0530

    sample.cpp

---
+++ pair/sample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // to store a cartesian pair
+    pair<int, int> p1;
+
+    p1.first = 10;
+    p1.second = 20;
+
+    cout << p1.first << " " << p1.second << endl;
+
+    return 0;
+}
