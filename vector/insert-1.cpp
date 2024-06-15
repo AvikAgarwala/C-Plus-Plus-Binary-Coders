d805140375ceb5a2160f791e7b2ff2f1f2ed6cd3
Alik Agarwala <alikagarwala@outlook.com>
2024-06-15 23:43:54 +0530

    insert-1.cpp

---
+++ vector/insert-1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+    // 1 2 3 4 5
+
+    v.insert(v.begin(), 7);
+
+    // 7 1 2 3 4 5
+
+    for (int i : v)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    v.insert(v.begin() + 1, 6);
+    // 7 6 1 2 3 4 5
+
+    v.insert(v.end(), 10);
+
+    // 7 6 1 2 3 4 5 10
+
+    for (int i : v)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
